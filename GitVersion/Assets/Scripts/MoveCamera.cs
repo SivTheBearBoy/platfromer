c37d88c7fab3dfe058c71dd45dd1648bd829a932
Siv <sivgames904@gmail.com>
2022-10-09 01:04:36 +0200

    Added assets
    Scripts, Environments, Created scene, Added player, Added textures

---
+++ GitVersion/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform player;
+
+    void Update()
+    {
+        transform.position = player.transform.position;
+    }
+}
