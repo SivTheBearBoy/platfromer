0c18e6596f96dbbc68105572060e6656f0fe52f1
ReasonOfLife <89902003+AlexanderRuijter@users.noreply.github.com>
2022-12-05 20:45:02 +0100

    - Game update
    - Added dialog system

---
+++ GitVersion/Assets/Scripts/UI/Dialog.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float TextSpeed;
+
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialog();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.E))
+        {
+            if (textComponent.text == lines[index]){
+                nextLine();
+            }
+            else{
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    void StartDialog()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(TextSpeed);
+        }
+    }
+
+    void nextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else{
+            gameObject.SetActive(false);
+        }
+    }
+}
